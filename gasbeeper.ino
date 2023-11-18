0a1fb7a5bb8a6ae314130ff5b0b2841697b4db60
devanarayanana787 <138719053+devanarayanana787@users.noreply.github.com>
2023-11-19 01:55:50 +0530

    Create gasbeeper.ino

---
+++ gasbeeper.ino
@@ -0,0 +1,27 @@
+#define BUZZER_PIN 3
+#define light 5
+
+void setup() 
+{
+  pinMode(BUZZER_PIN, OUTPUT);
+  //Serial.begin(9600);
+}
+
+void loop() 
+{
+  int sensorValue = analogRead(A0);
+  if (sensorValue > 300)
+  {
+    digitalWrite(BUZZER_PIN, 50);
+    digitalWrite(light, HIGH);
+    delay(1000);
+    digitalWrite(light, LOW);
+
+  }
+  else
+  {
+    digitalWrite(BUZZER_PIN, 0);
+    digitalWrite(light, LOW);  
+  }
+  delay(1000);
+} 
